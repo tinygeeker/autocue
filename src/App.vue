af14ff9843295bbc8497e6de81e256b49294e812
root <root@JP01.mhmmdnx0xsze3llrwqm4gd0cce.lx.internal.cloudapp.net>
2023-06-20 01:58:03 +0000

    init

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,7 @@
 <template>
   <el-card>
     <el-row type="flex" justify="center">
-      <el-col :span="2">
-        <el-menu 
-          router
-          background-color="#545c64" 
-          text-color="#ffffff"
-          :default-active="$route.path"
-          style="height: 750px;"
-        >
-          <el-menu-item index="/">
-            首页/生成器
-          </el-menu-item>
-          <el-menu-item index="/donate">
-            打赏/改需求
-          </el-menu-item>
-          <el-menu-item index="/group">
-            交流/进群聊
-          </el-menu-item>
-        </el-menu>
-      </el-col>
+
       <el-col :span="22">
         <router-view></router-view>
       </el-col>
